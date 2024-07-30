6ff0c80d4fb7ee6632e50d5d8cceeeb8daf08fa6
Ddraigo <laithanh1000@gmail.com>
2024-07-30 18:45:43 +0700

    update

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class BallMove : MonoBehaviour
 {
 
-    public Vector3 moveDirection { get; private set; }
     public Rigidbody rd;
 
     [SerializeField] private float moveSpeed;
@@ -22,9 +21,9 @@ public class BallMove : MonoBehaviour
         //Vector3 direction = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
         //transform.position += direction * moveSpeed * Time.fixedDeltaTime;
 
-        Vector3 rollDirec = new Vector3(Joystick.Instance.Horizontal, 0, Joystick.Instance.Vertical);
-        rd.AddForce(rollDirec * moveSpeed);
-        
+        Vector3 rollDirection = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
+        rd.AddForce(rollDirection * moveSpeed);
+
     }
 
 
