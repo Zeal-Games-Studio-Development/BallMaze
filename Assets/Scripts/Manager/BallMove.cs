292fb94e13326d3a1492c1b074ba854b012968d7
Ddraigo <laithanh1000@gmail.com>
2024-08-01 17:58:02 +0700

    Update map

--- Assets/Scripts/Manager/BallMove.cs
+++ Assets/Scripts/Manager/BallMove.cs
@@ -1,50 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEditor.Rendering;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class BallMove : MonoBehaviour
 {
     public static BallMove Instance;
     public Rigidbody rd;
 
+    [SerializeField] private ForceMode forceMode;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float powerJump;
     [SerializeField] private int score;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float rayDistance;
+
+    private Vector3 oldCheckPoint;
 
     private void Awake()
     {
         Instance = this;
+        oldCheckPoint = transform.position;
         LoadScore();
     }
 
     private void FixedUpdate()
     {
         Move();
-        
+        OnGroud();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+        ReturnCheckPoint();
     }
 
     private void Move()
     {
-        //Vector3 direction = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
+        Vector3 direction = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
         //transform.position += direction * moveSpeed * Time.fixedDeltaTime;
 
-        Vector3 rollDirection = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
-        rd.AddForce(rollDirection * moveSpeed);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 inputDirection = new Vector3(horizontalInput, 0, verticalInput) + direction;
+
+  
+        if (inputDirection.magnitude > 1)
+        {
+            inputDirection.Normalize();
+        }
+
+
+        rd.AddForce(inputDirection * moveSpeed);
     }
 
     public void Jump()
     {
-        rd.AddForce(Vector3.up * powerJump, ForceMode.Impulse);
-
+        if (isGrounded)
+        {
+            rd.AddForce(Vector3.up * powerJump, forceMode);
+            isGrounded = false; 
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        score++;
-        SaveScore();
-        Debug.Log("GET SCORE: " + score);
+
+        if (other.gameObject.tag == "Barrier")
+        {
+
+            transform.position = oldCheckPoint;
+        }
+        else
+        {
+            score++;
+            SaveScore();
+            Debug.Log("GET SCORE: " + score);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+
+    private void OnGroud()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void ReturnCheckPoint()
+    {
+        if(transform.position.y <= -5)
+        {
+            transform.position = oldCheckPoint;
+            rd.AddForce(Vector3.zero);
+        }
+        
     }
 
     private void SaveScore()
--- Assets/Scripts/Manager/CameraFollow.cs
+++ Assets/Scripts/Manager/CameraFollow.cs
@@ -9,17 +9,13 @@ public class CameraFollow : MonoBehaviour
     private float XAxit;
     private float YAxit;
     [SerializeField] private Vector3 offset;
-    [SerializeField] private float rotationSpeed = 5f;
-    public Joystick Joystick;
+
 
     private void FixedUpdate()
     {
         //YAxit += Input.GetAxis("Mouse X");
         //XAxit -= Input.GetAxis("Mouse Y");
-        Vector3 dir = new Vector3(Joystick.Instance.Direction.x, 0, Joystick.Instance.Direction.y);
-        transform.position = target.transform.position + offset + dir;
-
-        transform.rotation = Quaternion.LookRotation(dir * Time.fixedDeltaTime);
+        transform.position = target.transform.position + offset;
         transform.LookAt(transform.position);
     }
 
